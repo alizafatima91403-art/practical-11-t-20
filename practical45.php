277eaef4d3861e0eb7b939ec61d1a1a7f2fd4ede
alizafatima91403-art <alizafatima91403@gmail.com>
2025-09-09 10:21:30 -0400

    Add files via upload

---
+++ practical45.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Online Quiz</title>
+</head>
+<body>
+    <h2>Online Quiz</h2>
+    <form method="POST" action="">
+        <p>1. PHP stands for?</p>
+        <input type="radio" name="q1" value="a"> Personal Home Page<br>
+        <input type="radio" name="q1" value="b"> Private Hy
---
+++ practical46.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact Form</title>
+</head>
+<body>
+    <h2>Contact Us</h2>
+    <form method="GET" action="thankyou.php">
+        <label>Name:</label>
+        <input type="text" name="name"><br><br>
+
+        <label>Subject:</label>
+        <input type="text" name="subject"><br><br>
+
+        <label>Message:</label><br>
+        <textarea name="message" rows="5" cols="30"></textarea><br><br>
+
+        <input type="submit" value="Send">
+    </form>
+</body>
+</html>
